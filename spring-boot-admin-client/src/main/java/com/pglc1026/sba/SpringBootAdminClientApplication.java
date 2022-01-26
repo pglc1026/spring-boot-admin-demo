020b25ab79467e1e562e82da754cb3edb62e1be5
刘畅 <pglc1026@liuchangdeMacBook-Pro.local>
2022-01-26 10:56:20 +0800

    init

---
+++ spring-boot-admin-client/src/main/java/com/pglc1026/sba/SpringBootAdminClientApplication.java
@@ -0,0 +1,16 @@
+package com.pglc1026.sba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Will Liu
+ * @date 2022/1/26
+ */
+@SpringBootApplication
+public class SpringBootAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminClientApplication.class);
+    }
+}
---
+++ spring-boot-admin-server/src/test/java/com/pglc1026/sba/SpringBootAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pglc1026.sba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
